bc6084ce02ee2be24f627abfdfbe76db8239bb04
Adam Imdieke <adam.imdieke3691@gmail.com>
2022-11-18 20:38:17 -0600

    Added WebSockets server

--- firmware/src/main.cpp
+++ firmware/src/main.cpp
@@ -2,9 +2,9 @@
 #include <DNSServer.h>
 #include <ESP8266WiFi.h>
 #include <LittleFS.h>
+#include <WebSocketsServer.h>
 
 #include "HTTP_server.h"
-
 //#include <ESP8266mDNS.h>
 // -----------------------------
 
@@ -17,12 +17,23 @@ const IPAddress localhost = IPAddress(192, 168, 0, 1);
 const IPAddress subnet = IPAddress(255, 255, 0, 0);
 
 DNSServer DNS_server;
-
+WebSocketsServer WS_server(81,"/", "arduino");
 WiFiServer HTTP_server(localhost,80);
 
 // TCP/WEBSOCKETS SERVER
 
 //------------------------------
+
+void WS_event(uint8_t remote_num, WStype_t ev_type, uint8_t* data, size_t len) {
+    switch (ev_type) {
+    case WStype_CONNECTED:
+        break; // TODO: LOGGING/INDI
+    case WStype_DISCONNECTED:
+        break; // TODO: LOGGING/INDI
+    case WStype_TEXT:
+        Serial.write(data, len);
+    }
+}
 
 void setup() {
     Serial.begin(9600);
@@ -35,6 +46,9 @@ void setup() {
 
     DNS_server.setErrorReplyCode(DNSReplyCode::NoError);
     DNS_server.start(53, "*", localhost);
+
+    WS_server.begin();
+    WS_server.onEvent(WS_event);
     
     HTTP_server.begin();
     HTTP_init();
@@ -44,6 +58,6 @@ void setup() {
  
 void loop() {
     HTTP_loop(&HTTP_server);
+    WS_server.loop();
     DNS_server.processNextRequest();
-    
 }
